5c6079de66c05c6f292ead1e08e98d8a5351c1ac
Ömer Ayhan <om.ayhan247@gmail.com>
2021-08-29 01:35:52 +0300

    improvements on added guess control

--- Hangman Bot/Hangman_bot.py
+++ Hangman Bot/Hangman_bot.py
@@ -5,7 +5,8 @@ import hangman_shape
 intents = discord.Intents(messages=True, guilds=True, reactions=True, members=True, presences=True)
 bot_main = commands.Bot(command_prefix='!',intents=intents)
 bot_main.channel_name="test-chat"
-bot_main.guess_word=" "
+bot_main.game_word=""
+bot_main.guess_word=""
 bot_main.attempt=0
 with open("TOKEN_KEY.json") as key:
     token_data = json.load(key)
@@ -20,20 +21,42 @@ async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandNotFound):
         await ctx.send(f"Unknown command {user}")
 
+    
+
+@bot_main.command()
+async def set_guess(ctx, word):
+    channel = discord.utils.get(ctx.guild.text_channels, name=bot_main.channel_name)
+    user = ctx.author.mention
+    bot_main.game_word = word.strip()
+    bot_main.game_word = word.guessp()
+
+    await channel.send(f"{user} set the word")
+    
+    
 @bot_main.command()
 async def guess(ctx,answer):
     channel = discord.utils.get(ctx.guild.text_channels, name=bot_main.channel_name)
     user = ctx.author.mention
     print(answer)
     if len(answer)==1:
-        if answer in bot_main.guess_word:
-            await channel.send(f"'{answer}' guessed one:star_struck:  {user}")
+        if len(bot_main.game_word)>1:
+            if (answer in bot_main.guess_word):
+                bot_main.guess_word += answer             
+                await channel.send(f"'{answer}' guessed one:star_struck:  {user}")
+                if bot_main.guess_word==bot_main.game_word:
+                    await channel.send(f"'{user} won the game :star_struck:'")
+                    bot_main.game_word=""
+                    bot_main.guess_word=""
+                    
+                return
+            else:
+                await channel.send(f"'{answer}' wrong:japanese_ogre:  {user}")
+                print(bot_main.attempt)
+                await channel.send(f"`{hangman_shape.HANGMAN_PICS[bot_main.attempt]}`")
+                bot_main.attempt += 1
+                await channel.send(f"{7-bot_main.attempt} attempts remaining")
         else:
-            await channel.send(f"'{answer}' wrong:japanese_ogre:  {user}")
-            print(bot_main.attempt)
-            await channel.send(f"`{hangman_shape.HANGMAN_PICS[bot_main.attempt]}`")
-            bot_main.attempt += 1
-            await channel.send(f"{7-bot_main.attempt} attempts remaining")
+            await channel.send(f"please set a word to guess first {user}")
     else:
         await channel.send(f"only one word {user}")
         
