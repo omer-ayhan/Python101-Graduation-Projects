2694fce84538bb22f02e5d2ad034dc3e86eac36d
Ömer Ayhan <om.ayhan247@gmail.com>
2021-08-28 23:26:31 +0300

    hangman bot started implementing word check system and printing hang shape

--- Hangman Bot/Hangman_bot.py
+++ Hangman Bot/Hangman_bot.py
@@ -5,13 +5,13 @@ intents = discord.Intents(messages=True, guilds=True, reactions=True, members=Tr
 bot_main = commands.Bot(command_prefix='!',intents=intents)
 bot_main.channel_name="test-chat"
 bot_main.guess_word="test"
+bot_main.lives=5
 with open("TOKEN_KEY.json") as key:
     token_data = json.load(key)
 TOKEN = token_data['TOKEN_HANGMAN_GAME']
 @bot_main.event
 async def on_ready():
     print("Hangman Bot İnitializing...")
-
 @bot_main.event
 async def on_command_error(ctx, error):
     user = ctx.author.mention
@@ -20,22 +20,28 @@ async def on_command_error(ctx, error):
 
 @bot_main.command()
 async def guess(ctx,args):
-    if args.length==1:
-        channel = discord.utils.get(ctx.guild.text_channels, name=bot_main.channel_name)
-        user = ctx.author.mention
-        # for i in list(args):
-        #     str_word=str_word.join(i)
-        print(args)
-        if args in bot_main.guess_word:
-            await channel.send("guessed")
-        else:
-            await channel.send("not guessed")
-        
-@guess.error
-async def pomodoro_error(ctx, error):
-    user = ctx.author.mention
+    lives_left=bot_main.lives
     channel = discord.utils.get(ctx.guild.text_channels, name=bot_main.channel_name)
-    if isinstance(error, commands.CommandInvokeError):
-        await channel.send(f"Please enter only one word {user}")
+    user = ctx.author.mention
+    print(args)
+    if args in bot_main.guess_word:
+        await channel.send("guessed")
+    else:
+        print(lives_left)
+        await channel.send("not guessed")
+        # str_hang=f"`{' '*4}_____\n{' '*4}|{' '*4}|\n{' '*4}|{' '*4}\n{' '*4}|{' '*4}\n{' '*4}|{' '*4}\n{' '*4}|{' '*4}\n{' '*4}|{' '*4}\n ___|___{' '*4}`"
+        str_hang=f"`{' '*4}_____\n`"
+        for i in range(6):
+            str_hang+=f"`{' '*2}|{' '*4}{lives_left if (lives_left<=5 and lives_left>3) else ' ' }\n`"
+            lives_left-=1
+        await channel.send(str_hang)
+        
+        
+# @guess.error
+# async def pomodoro_error(ctx, error):
+#     user = ctx.author.mention
+#     channel = discord.utils.get(ctx.guild.text_channels, name=bot_main.channel_name)
+#     if isinstance(error, commands.CommandInvokeError):
+#         await channel.send(f"Please enter only one word {user}")
 
 bot_main.run(TOKEN)
