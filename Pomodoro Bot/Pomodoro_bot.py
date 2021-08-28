c651b390f1cd861e849bfd367e06071d9911bf96
Ömer Ayhan <om.ayhan247@gmail.com>
2021-08-28 21:04:29 +0300

    changed to english

--- Pomodoro Bot/Pomodoro_bot.py
+++ Pomodoro Bot/Pomodoro_bot.py
@@ -17,16 +17,16 @@ def check_cmd(msg):
         return msg
 @bot.event
 async def on_ready():
-    print("Pomodoro Bot Başlatılıyor...")
+    print("Pomodoro Bot İnitializing...")
 
 @bot.event
 async def on_command_error(ctx, error):
     user = ctx.author.mention
     if isinstance(error, commands.CommandNotFound):
-        await ctx.send(f"Böyle bir komut bulunmamakta {user}")
+        await ctx.send(f"Unknown command {user}")
 
 @bot.command()
-async def p_başlat(ctx,s1,s2):
+async def p_start(ctx,s1,s2):
     channel = discord.utils.get(ctx.guild.text_channels, name=bot.channel_name)
     user = ctx.author.mention
     s1 = int(s1) * 60
@@ -36,28 +36,28 @@ async def p_başlat(ctx,s1,s2):
     s2_str = int(s2/60)
     print(s1_str, s2_str)
     bot.stop_timer = True
-    str = f"{s1_str} dakika ders\n{s2_str} dakika dinlenme {user}"
+    str = f"{s1_str} minutes study time\n{s2_str} minutes break time {user}"
     await channel.send(str)
     print(bot.stop_timer)
     while bot.stop_timer:
         check_cmd(await asyncio.sleep(s1))
-        await channel.send(check_cmd(f"Ders süresi bitti:laughing: {s2_str} dakika dinlenme vakti :sunglasses: {user}"))
+        await channel.send(check_cmd(f"Study time over:laughing: {s2_str} minutes break time :sunglasses: {user}"))
         check_cmd(await asyncio.sleep(s1))
-        await channel.send(check_cmd(f"Dinlenme süresi bitti:weary: {s1_str} dakika ders vakti :nerd: {user}"))
+        await channel.send(check_cmd(f"Break time over:weary: {s1_str} minutes study time :nerd: {user}"))
 
 @bot.command()
 async def p_durdur(ctx):
     bot.stop_timer=False
     user = ctx.author.mention
     channel = discord.utils.get(ctx.guild.text_channels, name=bot.channel_name)
-    await channel.send(f"Pomodoro durduruldu {user}")
+    await channel.send(f"Pomodoro stopped {user}")
     
-@p_başlat.error
+@p_start.error
 async def pomodoro_error(ctx, error):
     user = ctx.author.mention
     channel = discord.utils.get(ctx.guild.text_channels, name=bot.channel_name)
     if isinstance(error, commands.MissingRequiredArgument):
-        await channel.send(f"Lütfen komudu tam haliyle yazınız {user}")
+        await channel.send(f"Please write all arguments {user}")
     elif isinstance(error, commands.CommandInvokeError):
-        await channel.send(f"Lütfen sayı giriniz {user}")
+        await channel.send(f"Please enter all arguments as numbers {user}")
 bot.run(TOKEN)
